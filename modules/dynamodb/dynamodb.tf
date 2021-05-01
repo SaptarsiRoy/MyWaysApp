41f5c31bd283bf204396c9874ff7155244558c15
SaptarsiRoy <saptarsiroy12@gmail.com>
2021-05-02 02:11:07 +0530

    code for dynamodb

---
+++ modules/dynamodb/dynamodb.tf
@@ -0,0 +1,32 @@
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "GameScores"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "Name"
+    type = "S"
+  }
+
+  attribute {
+    name = "description"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = false
+  }
+
+  tags = {
+    Name        = "dynamodb-table-1"
+    Environment = "production"
+  }
+}
