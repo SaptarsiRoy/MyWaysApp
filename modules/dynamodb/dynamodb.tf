263547a1673e4cab5a644722841721fe8dd7fe6e
SaptarsiRoy <saptarsiroy12@gmail.com>
2021-05-02 02:12:21 +0530

    changed table name

--- modules/dynamodb/dynamodb.tf
+++ modules/dynamodb/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = "GameScores"
+  name           = "My_Ways"
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
@@ -26,7 +26,7 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
   }
 
   tags = {
-    Name        = "dynamodb-table-1"
+    Name        = "myways"
     Environment = "production"
   }
 }
