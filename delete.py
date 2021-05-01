6b7bca7d0690e4e3123f375294b8a12ab5a5ad1d
SaptarsiRoy <saptarsiroy12@gmail.com>
2021-05-01 13:12:42 +0530

    created code to delete items

---
+++ delete.py
@@ -0,0 +1,28 @@
+import json
+import boto3
+
+
+def lambda_handler(event, context):
+    parameter = event['queryStringParameters']
+    
+    database = boto3.resource('dynamodb')
+    table = database.Table('MyWays')
+    
+    rp = table.get_item(Key={
+        'id': parameter["id"]
+        }
+    )
+    if 'Item' not in rp:
+        x = "Candidate Not Present"
+        
+    else:
+        rp1 = table.delete_item(Key={
+               'id': parameter['id']
+               }
+            )
+        if rp1['ResponseMetadata']['HTTPStatusCode'] == 200:
+                x = "Candidate Deleted Successfully"
+    return {
+        'statusCode': 200,
+        'body': json.dumps(x)
+    }
