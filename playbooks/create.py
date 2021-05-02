a3fbd2babea2a3b6d0d18b28529d9fe1ba7a47ea
Saptarsi Roy <saptarsiroy12@gmail.com>
2021-05-02 19:44:08 +0530

    update code

--- playbooks/create.py
+++ playbooks/create.py
@@ -1,33 +1,19 @@
 #!/bin/python3
-import  cgi
+import cgi
 import requests
 
 
 form = cgi.FieldStorage()
 c_key = form.getvalue('id')
-error = False
-content = None
-lower_alpha_list = [chr(x) for x in range(ord('a'), ord('z') + 1)]
-upper_alpha_list = [chr(x) for x in range(ord('A'), ord('Z') + 1)]
-for i in c_key:
-    if i in lower_alpha_list or i in upper_alpha_list:
-        error = True
-        break
-
-if not error:
-    name = form.getvalue('name')
-    desc = form.getvalue('description')
-    url = '{{ create_api_endpoint }}?id='
-
-    url = url+c_key + '&name=' + name + '&description=' + desc
-    res = requests.get(url)
-    content = res.content
-    content = content.decode('ascii')
-else:
-    content = "Enter Valid Candidate ID"
+name = form.getvalue('name')
+desc = form.getvalue('description')
+url = '{{ create_api_endpoint }}?id='
+url = url+c_key + '&name=' + name + '&description=' + desc
+res = requests.get(url)
 
 print("Content-Type: text/html")
 print()
 
-
+content = res.content
+content = content.decode('ascii')
 print(f"<h1><center>{ content } </center></h1>")
--- playbooks/search.py
+++ playbooks/search.py
@@ -11,4 +11,13 @@ res = requests.get(url)
 
 print("Content-Type: text/html")
 print()
-print(res.json())
+
+content = res.content
+content = content.decode('ascii').split(",")
+
+if 'valid' not in content[0]:
+    print(f"<h1><center>Id: { content[0] } </center></h1>")
+    print(f"<h1><center>Name: { content[1] } </center></h1>")
+    print(f"<h3><center>Description: { content[2] } </center></h3>")
+else:
+    print(f"<h1><center>{ content[0] } </center></h1>")
